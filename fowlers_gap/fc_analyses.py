af7a3ffc342dd50455255614b2cc9493cab1499a
Adrian Fisher <75593398+adrian-g-fisher@users.noreply.github.com>
2022-02-28 14:07:21 +1100

    updated fowlers fc analysis

---
+++ fowlers_gap/fc_analyses.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python
+"""
+
+This analyses a variety of fractional cover data for Fowlers Gap
+
+"""
+
+
+import os
+import sys
+import numpy as np
+import ternary
+import matplotlib
+import matplotlib.pyplot as plt
+
+plt.rcParams['font.family'] = 'Arial'
+plt.rcParams['figure.dpi'] = 200
+plt.rcParams['figure.figsize'] = (4, 3.7)
+
+# Read in star transect data 
+idList = []
+idYearList = []
+fc_data = []
+colours = []
+with open("fg_star_transects.csv", 'r') as f:
+    f.readline()
+    for line in f:
+        ID = line.strip().split(',')[0]
+        Year = line.strip().split(',')[2][0:4]
+        idYearList.append('%s_%s'%(ID, Year))
+        PV = float(line.strip().split(',')[20])
+        NPV = float(line.strip().split(',')[21])
+        Bare = float(line.strip().split(',')[22])
+        fc_data.append([PV, NPV, Bare])
+        colours.append([Bare, PV, NPV]) # red, green, blue
+fc_data = np.array(fc_data) * 100
+idYearList = np.array(idYearList)
+colours = np.array(colours)
+
+# Read in landsat fc data
+NAT_idYearList = []
+NAT_fc_data = []
+with open("seasonal_fc_extract.csv", 'r') as f:
+    f.readline()
+    for line in f:
+        ID = line.strip().split(',')[0]
+        Year = line.strip().split(',')[1].split('_')[2][1:5]
+        NAT_idYearList.append('%s_%s'%(ID, Year))
+        PV = float(line.strip().split(',')[5])
+        NPV = float(line.strip().split(',')[7])
+        Bare = float(line.strip().split(',')[3])
+        NAT_fc_data.append([PV, NPV, Bare])
+NAT_idYearList = np.array(NAT_idYearList)
+NAT_fc_data = np.array(NAT_fc_data)
+
+# Read in AZN landsat fc data
+AZN_idYearList = []
+AZN_fc_data = []
+with open("seasonal_AZN_fc_extract.csv", 'r') as f:
+    f.readline()
+    for line in f:
+        ID = line.strip().split(',')[0]
+        Year = line.strip().split(',')[1].split('_')[2][1:5]
+        AZN_idYearList.append('%s_%s'%(ID, Year))
+        PV = float(line.strip().split(',')[5])
+        NPV = float(line.strip().split(',')[7])
+        Bare = float(line.strip().split(',')[3])
+        AZN_fc_data.append([PV, NPV, Bare])
+AZN_idYearList = np.array(AZN_idYearList)
+AZN_fc_data = np.array(AZN_fc_data)
+
+# Check that field and satellite data are in the same order
+for i, IDY in enumerate(idYearList):
+    if (IDY != NAT_idYearList[i]) or (IDY != AZN_idYearList[i]):
+        print("Data does not line up")
+
+# Remove some sites as they have no data in the AZN model
+fc_data = fc_data[(idYearList != '3_2012') & (idYearList != '4_2012') &
+                  (idYearList != '8_2013') & (idYearList != '9_2013'), :]
+NAT_fc_data = NAT_fc_data[(idYearList != '3_2012') & (idYearList != '4_2012') &
+                         (idYearList != '8_2013') & (idYearList != '9_2013'), :]
+AZN_fc_data = AZN_fc_data[(idYearList != '3_2012') & (idYearList != '4_2012') &
+                         (idYearList != '8_2013') & (idYearList != '9_2013'), :]
+colours = colours[(idYearList != '3_2012') & (idYearList != '4_2012') &
+                         (idYearList != '8_2013') & (idYearList != '9_2013'), :]
+
+# Make ternary plot of field measurements
+colours = np.array(colours)
+fontsize = 12
+offset = 0.15
+fig, tax = ternary.figure(scale=100)
+tax.boundary(linewidth=1)
+tax.gridlines(color="grey", multiple=20, linewidth=0.5)
+tax.left_axis_label("Bare soil and rock", offset=offset)
+tax.right_axis_label("Non-photosynthetic vegetation", offset=offset)
+tax.bottom_axis_label("Photosynthetic vegetation", offset=offset)
+tax._redraw_labels()
+tax.ticks(axis='lbr', linewidth=1, multiple=20, offset=0.02)
+tax.set_background_color(color="white")
+tax.clear_matplotlib_ticks()
+tax.get_axes().axis('off')
+tax.scatter(fc_data, marker='o', c=colours)
+plt.savefig("ternary.png")
+plt.clf()
+
+# Make plot of field vs satellite data
+# Make plots
+fig = plt.figure(1)
+fig.set_size_inches((6, 4))
+rect = [[[0.17, 0.50, 0.2, 0.3],[0.40, 0.50, 0.2, 0.3],[0.63, 0.50, 0.2, 0.3]],
+        [[0.17, 0.15, 0.2, 0.3],[0.40, 0.15, 0.2, 0.3],[0.63, 0.15, 0.2, 0.3]]]
+
+ylabels = ["National model", "Arid model"]
+data = [NAT_fc_data, AZN_fc_data]
+
+for row in range(2):
+    for col in range(3):
+    
+        xdata = fc_data[:, col] / 100.0
+        ydata = data[row][:, col] / 100.0
+        
+        ax = plt.axes(rect[row][col])
+        ax.set_ylim((-0.05, 1.05))
+        ax.set_xlim((-0.05, 1.05))
+        ax.plot(xdata, ydata, marker="o", lw=0, markeredgecolor="0.7",
+                markerfacecolor="none", markersize=3)
+        ax.plot([-0.05, 1.05], [-0.05, 1.05], ls='-', c='k', lw='1')
+        ax.set_xticks([0.0, 0.5, 1.0])
+        ax.set_yticks([0.0, 0.5, 1.0])
+        if row == 1:
+            ax.set_xticklabels([0.0, 0.5, 1.0], fontsize=8)
+            if col == 1:
+                ax.set_xlabel("Field measured cover", fontsize=8)
+        else:
+            ax.set_xticklabels([])
+        if col == 0:
+            ax.set_ylabel(ylabels[row], fontsize=8)
+            ax.set_yticklabels([0.0, 0.5, 1.0], fontsize=8)
+        else:
+            ax.set_yticklabels([])
+        if row == 0:
+            if col == 0:
+                ax.set_title("Photosynthetic\nvegetation", fontsize=8)
+            if col == 1:
+                ax.set_title("Non-photosynthetic\nvegetation", fontsize=8)
+            if col == 2:
+                ax.set_title("Bare soil\nand rock", fontsize=8)
+        error = ydata - xdata
+        rmse = np.sqrt(np.mean((error)**2))
+        n = ydata.size
+        ax.text(0.02, 0.90, "RMSE = %.2f"%rmse, fontsize=8)
+        ax.text(0.02, 0.78, "n = %i"%n, fontsize=8)
+plt.savefig("validation.png")
+plt.clf()
---
+++ fowlers_gap/fc_star_extract.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+"""
+
+This extracts fractional cover data for the star transects at Fowlers Gap
+
+"""
+
+
+import os
+import sys
+import glob
+import numpy as np
+from osgeo import gdal, ogr
+from rios import applier
+from rios import rat
+from scipy import ndimage
+from datetime import datetime
+
+"""
+Extracts the mean and standard deviation of each fractional cover component
+(green, dead and bare) for each site polygon in the input shapefile. The
+shapefile needs to have an attribute called "Id" which has a unique integer for
+each site. It should also be in the same coordinate reference system as the
+image data, which is EPSG:3577, or in ArcGIS this is defined as
+GDA_1994_Australia_Albers.
+"""
+
+
+def getSuroundingPixels(info, inputs, outputs, otherargs):
+    """
+    Gets stats from the 8 pixels surrounding a certain site or sites 
+    """
+    sites = inputs.sites[0]
+    for idvalue in otherargs.idstoget:
+        singlesite = (sites == idvalue)
+        singlesite = ndimage.maximum_filter(singlesite, size=3)
+        fc = inputs.fc
+        nodataPixels = (fc[0] == 0)
+        fc = np.where(fc >= 100, fc - 100, 0)
+        bare = fc[0][singlesite]
+        green = fc[1][singlesite]
+        dead = fc[2][singlesite]
+        nodataPixels = np.sum(nodataPixels[singlesite])
+        with open(otherargs.csvfile, 'a') as f:
+            line = '%i,%s'%(idvalue, otherargs.imagename)
+            if nodataPixels == 0:
+                line = '%s,%i'%(line, bare.size)
+                line = '%s,%.2f,%.2f'%(line, np.mean(bare), np.std(bare))
+                line = '%s,%.2f,%.2f'%(line, np.mean(green), np.std(green))
+                line = '%s,%.2f,%.2f\n'%(line, np.mean(dead), np.std(dead))
+            else:
+                line = '%s,999'%line
+                line = '%s,999,999'%line
+                line = '%s,999,999'%line
+                line = '%s,999,999\n'%line
+            f.write(line)
+
+
+def extract_pixels(pointfile, imagefile, csvfile, idstoget):
+    """
+    This sets up RIOS to extract pixel statistics for the points.
+    """
+    # Extract the pixels
+    infiles = applier.FilenameAssociations()
+    outfiles = applier.FilenameAssociations()
+    otherargs = applier.OtherInputs()
+    controls = applier.ApplierControls()
+    controls.setBurnAttribute("Id")
+    controls.setReferenceImage(imagefile)
+    controls.setFootprintType(applier.BOUNDS_FROM_REFERENCE)
+    controls.setWindowXsize(1200)
+    controls.setWindowYsize(1200)
+    infiles.sites = pointfile
+    infiles.fc = imagefile
+    otherargs.idstoget = idstoget
+    otherargs.csvfile = csvfile
+    otherargs.imagename = os.path.basename(imagefile)
+    applier.apply(getSuroundingPixels, infiles, outfiles,
+                  otherArgs=otherargs, controls=controls)
+
+
+# Read in dates for points
+ids = []
+sites = []
+dates = []
+with open("fg_star_transects.csv", 'r') as f:
+    f.readline()
+    for line in f:
+        ids.append(int(line.strip().split(',')[0]))
+        sites.append(line.strip().split(',')[1])
+        dates.append(line.strip().split(',')[2])
+ids = np.array(ids)
+sites = np.array(sites)
+dates = np.array(dates)
+seasonalDates = []
+for i, d in enumerate(dates):
+    year = int(d[0:4])
+    month = int(d[4:6])
+    if month in [1, 2]:
+        s = '%s12%s02'%(year-1, year)
+    elif month in [3, 4, 5]:
+        s = '%s03%s05'%(year, year)
+    elif month in [6, 7, 8]:
+        s = '%s06%s08'%(year, year)
+    elif month in [9, 10, 11]:
+        s = '%s09%s11'%(year, year)
+    elif month == 12:
+        s = '%s12%s02'%(year, year+1)
+    seasonalDates.append(s)
+seasonalDates = np.array(seasonalDates)
+
+# Get imageList
+imageDir = r'S:\fowlers_gap\imagery\landsat\seasonal_fractional_cover'
+imageList = []
+idList = []
+for d in np.unique(seasonalDates):
+    imageList.append(os.path.join(imageDir,
+                                  'lztmre_nsw_m%s_dima2_subset.tif'%d))         
+    idList.append(ids[seasonalDates == d])
+
+# Write the csvfile header
+csvfile = r'seasonal_fc_extract.csv'
+with open(csvfile, 'w') as f:
+    f.write('Id,image,pixels,'+
+            'meanBare,stdevBare,meanGreen,stdevGreen,meanDead,stdevDead\n')
+
+# Iterate over images and get pixel values
+pointFile = (r'C:\Users\Adrian\OneDrive - UNSW\Documents\Fowlers_Gap\star_transects\fg_star_transects_albers.shp')
+for i, imagefile in enumerate(imageList):
+    extract_pixels(pointFile, imagefile, csvfile, idList[i])

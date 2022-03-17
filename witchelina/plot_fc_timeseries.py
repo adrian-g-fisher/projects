437427f166d900ab428613af3c6950bae20a80ab
Adrian Fisher <75593398+adrian-g-fisher@users.noreply.github.com>
2022-03-17 12:22:39 +1100

    added witchelina project

---
+++ witchelina/plot_fc_timeseries.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+"""
+
+This plots the fractional cover time series for the areas of interest at
+Witchelina.
+
+"""
+
+
+import os
+import sys
+import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+                               
+params = {'text.usetex': False, 'mathtext.fontset': 'stixsans',
+          'xtick.direction': 'out', 'ytick.direction': 'out',
+          'font.sans-serif': 'Arial', 'font.family': 'sans-serif'}
+plt.rcParams.update(params)
+
+# Read in data
+dates = []
+Id = []
+green = []
+dead = []
+csvfile = r'seasonal_fc_extract.csv'
+with open(csvfile, 'r') as f:
+    #Id,date,pixels,meanBare,stdevBare,meanGreen,stdevGreen,meanDead,stdevDead
+    f.readline()
+    for line in f:
+        l = line.strip().split(',')
+        Id.append(int(l[0]))
+        dates.append(int(l[1]))
+        green.append([float(l[5]), float(l[6])])
+        dead.append([float(l[7]), float(l[8])])
+Id = np.array(Id)
+green = np.array(green)
+dead = np.array(dead)
+datetimes = np.array([datetime.date(year=int(str(d)[:4]),
+                                    month=int(str(d)[4:6]), day=15) +
+                                    datetime.timedelta(days=30) for d in dates])
+titles = ["Stony flat (Id=1)", "Stony flat (Id=2)",
+          "Stony downs (Id=3)", "Stony downs (Id=4)",
+          "Farina Road (Id=5)", "Farina Road (Id=6)"]          
+
+
+
+
+
+# Plot green and dead cover over time for the six sites
+fig = plt.figure(1)
+fig.set_size_inches((8, 10))
+rects  = [[0.1, 0.85, 0.85, 0.13],
+          [0.1, 0.70, 0.85, 0.13],
+          [0.1, 0.55, 0.85, 0.13],
+          [0.1, 0.40, 0.85, 0.13],
+          [0.1, 0.25, 0.85, 0.13],
+          [0.1, 0.10, 0.85, 0.13]]
+for i in range(6):
+
+    poly_id = i + 1
+
+    ax = plt.axes(rects[i])
+    ax.set_xlim((datetime.date(1988, month=1, day=1), datetime.date(2021, month=11, day=1)))
+    ax.set_xticks([datetime.date(1990, month=1, day=1), datetime.date(2000, month=1, day=1),
+                   datetime.date(2010, month=1, day=1), datetime.date(2020, month=1, day=1)])
+    ax.xaxis.set_major_locator(mdates.YearLocator(10))
+    ax.xaxis.set_minor_locator(mdates.YearLocator(1))
+    
+    if i < 5:
+        ax.set_xticklabels([])
+    else:
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+
+    if i == 3:
+        ax.set_ylabel('                Vegetation cover (%)', fontsize=14)   
+    ax.set_ylim((0, 75))
+    ax.text(datetime.date(1988, month=6, day=1), 60, titles[i], fontsize=14)
+
+    dates = datetimes[Id == poly_id]
+    inds = dates.argsort()
+    green_ts = green[(Id == poly_id), :]
+    green_ts = green_ts[inds]
+    dead_ts = dead[(Id == poly_id), :]
+    dead_ts = dead_ts[inds]
+    dates = dates[inds]
+    green_ts = np.ma.masked_equal(green_ts, 999)
+    dead_ts = np.ma.masked_equal(dead_ts, 999)
+    
+    lower_green = green_ts[:, 0] - green_ts[:, 1]
+    lower_green[lower_green < 0] = 0
+    upper_green = green_ts[:, 0] + green_ts[:, 1]
+    upper_green[upper_green > 100] = 100
+    ax.fill_between(dates, lower_green, upper_green, alpha=0.2, facecolor='g', linewidth=0.0, edgecolor='g')
+    ax.plot(dates, green_ts[:, 0], color='g', linewidth=1)
+    
+    lower_dead = dead_ts[:, 0] - dead_ts[:, 1]
+    lower_dead[lower_dead < 0] = 0
+    upper_dead = dead_ts[:, 0] + dead_ts[:, 1]
+    upper_dead[upper_dead > 100] = 100
+    ax.fill_between(dates, lower_dead, upper_dead, alpha=0.2, facecolor='b', linewidth=0.0, edgecolor='b')
+    ax.plot(dates, dead_ts[:, 0], color='b', linewidth=1)
+    
+# Put legend down the bottom for PV and NPV
+axLeg = plt.axes([0, 0, 1, 0.15], frameon=False)
+axLeg.set_xlim((0, 100))
+axLeg.set_ylim((0, 2))
+axLeg.set_xticks([])
+axLeg.set_yticks([])
+axLeg.plot([15.5, 16.5], [0.5, 0.5], ls='-', c='b', lw=10, alpha=0.3)
+axLeg.plot([14.7, 17.3], [0.5, 0.5], ls='-', c='b', lw=1)
+axLeg.text(19, 0.4, r'Non-photosynthetic vegetation', fontsize=14)
+axLeg.plot([60.5, 61.5], [0.5, 0.5], ls='-', c='g', lw=10, alpha=0.3)
+axLeg.plot([59.7, 62.3], [0.5, 0.5], ls='-', c='g', lw=1)
+axLeg.text(64, 0.4, r'Photosynthetic vegetation', fontsize=14)
+
+plt.savefig(r'timeseries.png', dpi=300)
---
+++ witchelina/possible_area_extracts.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+"""
+
+This extracts fractional cover data for the areas of interest at Witchelina
+
+"""
+
+
+import os
+import sys
+import glob
+import numpy as np
+from osgeo import gdal, ogr
+from rios import applier
+from rios import rat
+from scipy import ndimage
+from datetime import datetime
+
+"""
+Extracts the mean and standard deviation of each fractional cover component
+(green, dead and bare) for each polygon in the input shapefile. The
+shapefile needs to have an attribute called "Id" which has a unique integer for
+each site. It should also be in the same coordinate reference system as the
+image data, which is EPSG:3577, or in ArcGIS this is defined as
+GDA_1994_Australia_Albers.
+"""
+
+
+def getPixels(info, inputs, outputs, otherargs):
+    """
+    Gets stats from the 8 pixels surrounding a certain site or sites 
+    """
+    sites = inputs.sites[0]
+    for idvalue in range(1, 7):
+        singlesite = (sites == idvalue)
+        fc = inputs.fc
+        nodataPixels = (fc[0] == 0)
+        fc = np.where(fc >= 100, fc - 100, 0)
+        bare = fc[0][singlesite]
+        green = fc[1][singlesite]
+        dead = fc[2][singlesite]
+        nodata = nodataPixels[singlesite]
+        bare = bare[nodata == 0]
+        green = green[nodata == 0]
+        dead = dead[nodata == 0]
+        with open(otherargs.csvfile, 'a') as f:
+            line = '%i,%s'%(idvalue, otherargs.date)
+            if bare.size > 0:
+                line = '%s,%i'%(line, bare.size)
+                line = '%s,%.2f,%.2f'%(line, np.mean(bare), np.std(bare))
+                line = '%s,%.2f,%.2f'%(line, np.mean(green), np.std(green))
+                line = '%s,%.2f,%.2f\n'%(line, np.mean(dead), np.std(dead))
+            else:
+                line = '%s,999'%line
+                line = '%s,999,999'%line
+                line = '%s,999,999'%line
+                line = '%s,999,999\n'%line
+            f.write(line)
+
+
+def extract_pixels(polyfile, imagefile, csvfile):
+    """
+    This sets up RIOS to extract pixel statistics for the points.
+    """
+    # Extract the pixels
+    infiles = applier.FilenameAssociations()
+    outfiles = applier.FilenameAssociations()
+    otherargs = applier.OtherInputs()
+    controls = applier.ApplierControls()
+    controls.setBurnAttribute("Id")
+    controls.setReferenceImage(imagefile)
+    controls.setFootprintType(applier.BOUNDS_FROM_REFERENCE)
+    controls.setWindowXsize(4500)
+    controls.setWindowYsize(4500)
+    infiles.sites = polyfile
+    infiles.fc = imagefile
+    otherargs.csvfile = csvfile
+    otherargs.date = os.path.basename(imagefile).split('_')[2][1:]
+    applier.apply(getPixels, infiles, outfiles,
+                  otherArgs=otherargs, controls=controls)
+
+
+# Get imageList
+imageDir = r'D:\witchelina\seasonal_fractional_cover\dim'
+imageList = glob.glob(os.path.join(imageDir, "*.tif"))
+
+# Write the csvfile header
+csvfile = r'seasonal_fc_extract.csv'
+with open(csvfile, 'w') as f:
+    f.write('Id,date,pixels,'+
+            'meanBare,stdevBare,meanGreen,stdevGreen,meanDead,stdevDead\n')
+
+# Iterate over images and get pixel values
+polyFile = (r'C:\Users\Adrian\OneDrive - UNSW\Documents\witchelina\exclosures\possible_areas.shp')
+for imagefile in imageList:
+    extract_pixels(polyFile, imagefile, csvfile)

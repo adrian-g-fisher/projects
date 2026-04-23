ff5e32b3f13532e7b09684d019a864cc1356c5f6
Adrian Fisher <adrian.fisher@unsw.edu.au>
2026-04-23 11:02:12 +1000

    Update extract_modisfc.py

--- sturt/extract_modisfc.py
+++ sturt/extract_modisfc.py
@@ -6,19 +6,21 @@ component (green, dead and bare) and saves it to a CSV file.
 
 The poygon shapefile needs to have an integer attribute called Id and should be
 in the same projection as the MODIS imagery (WGS84). MODIS data for
-Australia are located in S:\aust\modis_fractional_cover
+Australia are located in S:/aust/modis_fractional_cover
 """
 
 
 import os
 import sys
 import glob
+import math
 import numpy as np
 from osgeo import gdal, ogr
 from rios import applier
 from rios import rat
 from scipy import ndimage
 from datetime import datetime
+ogr.UseExceptions()
 
 
 def getFC(info, inputs, outputs, otherargs):
@@ -53,7 +55,7 @@ def getFC(info, inputs, outputs, otherargs):
         otherargs.results.append(line)
         
 
-def extract_fc(polyfile, imagefile, results):
+def extract_fc(imagefile, results, tempFile, xSize, ySize):
     """
     This sets up RIOS to extract pixel statistics for fractional cover.
     """
@@ -62,32 +64,63 @@ def extract_fc(polyfile, imagefile, results):
     outfiles = applier.FilenameAssociations()
     otherargs = applier.OtherInputs()
     controls = applier.ApplierControls()
-    controls.setBurnAttribute("Id")
-    controls.setReferenceImage(imagefile)
-    controls.setFootprintType(applier.BOUNDS_FROM_REFERENCE)
-    controls.setWindowXsize(4800)
-    controls.setWindowYsize(4800)
-    infiles.sites = polyfile
+    infiles.sites = tempFile
     infiles.fc = imagefile
     otherargs.results = results
-    otherargs.date = int(imagefile.replace(r".tif", "")[-6:])
+    year = imagefile.replace(r".tif", "").split(r".")[-4][1:]
+    month = imagefile.replace(r".tif", "").split(r".")[-3]
+    otherargs.date = int(year + month)    
+    controls.setFootprintType(applier.BOUNDS_FROM_REFERENCE)
+    controls.setReferenceImage(tempFile)
+    controls.setWindowXsize(xSize)
+    controls.setWindowYsize(ySize)
     applier.apply(getFC, infiles, outfiles, otherArgs=otherargs, controls=controls)
     return otherargs.results
 
-# Put all data into a list
-results = []
 
-# Fractional over (200101-202412)
+# Define inputs and outputs
+polyfile = r'C:\Users\z9803884\OneDrive - UNSW\Documents\student_projects\phd\matt_brun\polygon.shp'
+csvfile = r'C:\Users\z9803884\OneDrive - UNSW\Documents\student_projects\phd\matt_brun\polygon_fc_extract.csv'
+
+# Create temporary subset over polygon area
+srcFile = r'S:\aust\modis_fractional_cover\FC.v310.MCD43A4.A2026.02.aust.061.tif'
+tempFile = r'temp.tif'
+ds = ogr.Open(polyfile)
+for lyr in ds:
+    (xmin, xmax, ymin, ymax) = lyr.GetExtent()
+ds = None
+src_ds = gdal.Open(srcFile)
+dst_ds = gdal.Translate(tempFile, src_ds, bandList=[1], projWin=[xmin, ymax, xmax, ymin])
+dst_ds = None
+src_ds = None
+ds = gdal.Open(tempFile, gdal.GA_Update)
+band = ds.GetRasterBand(1)
+zeros = np.zeros((band.YSize, band.XSize), dtype=np.uint8)
+band.WriteArray(zeros)
+xSize = ds.RasterXSize
+ySize = ds.RasterYSize
+ds = None
+xSize = math.ceil(xSize/256.0) * 256
+ySize = math.ceil(ySize/256.0) * 256
+
+# Burn polygons into temp
+ds = gdal.Open(tempFile, gdal.GA_Update)
+ps = gdal.OpenEx(polyfile)
+gdal.Rasterize(ds, ps, attribute="Id")
+ds = None
+ps = None
+
+# Iterate over fractional over data
 imageDir = r'S:\aust\modis_fractional_cover'
 imageList = glob.glob(os.path.join(imageDir, '*.tif'))
-polyfile = r'polygons.shp'
+results = []
 for imagefile in imageList:
-    results = extract_fc(polyfile, imagefile, results)
-    print(imagefile, len(results))
+    results = extract_fc(imagefile, results, tempFile, xSize, ySize)
+    print(imagefile)
 results = np.array(results)
+os.remove(tempFile)
 
-# Write CSV
-csvfile = r'polygon_data_extract.csv'
+# Write CSV file
 with open(csvfile, 'w') as f:
     f.write('Id,date,modisPixels,meanBare,stdevBare,meanGreen,stdevGreen,meanDead,stdevDead\n')
     (rows, cols) = results.shape
